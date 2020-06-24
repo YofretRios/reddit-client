cd2d16338a43a0b838e7aeb32be2f53332e8fb11
Yofret Rios <yofret.rios@jobsity.com>
2020-06-24 12:42:17 -0500

    feat: side bar layout

---
+++ src/components/Post.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="py-2 flex">
+    <div>
+      <div class="overflow-hidden rounded thumbnail">
+        <img class="w-auto" src="http://via.placeholder.com/300" alt="thumbnail">
+      </div>
+    </div>
+    <div class="ml-2 flex-1">
+      <h3 class="text-sm font-bold">Post title</h3>
+
+      <p class="text-xs text-gray-500 pb-1">
+        Posted by <span>15 hours ago</span>
+      </p>
+
+      <footer class="relative flex items-center">
+        <p class="text-xs text-gray-500">36 comments</p>
+        <button type="button" class="btn btn-secondary sm ml-auto">Dismiss</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="hidden lg:block w-1/4">
-    <nav class="px-6 pt-6">
-      <div>
-        Post 1
-      </div>
-      <div>
-        Post 1
-      </div>
+    <nav class="px-4 pt-6 divide-y divide-gray-300">
+      <Post />
+      <Post />
     </nav>
   </div>
 </template>
 
 <script>
+import Post from './Post';
+
 export default {
   name: 'PostList',
+  components: {
+    Post
+  },
   data() {
-    return {
-
-    };
+    return {};
   },
 };
 </script>
