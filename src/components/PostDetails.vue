2092416f454690972ec66e097c036b80502206e5
Yofret Rios <yofret.rios@jobsity.com>
2020-06-24 13:42:33 -0500

    added nav bar

--- src/components/PostDetails.vue
+++ src/components/PostDetails.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="container w-full lg:w-3/4 xl:w-4/5">
-    <div class="py-6 px-6 lg:py-10 w-full">
-      <h1>Post Details 2</h1>
+    <header class="p-4 lg:px-6 lg:py-4 flex">
+      <button type="button" class="lg:hidden lg:px-3">
+        <img src="../assets/hamburger.svg" class="max-w-none w-auto">
+      </button>
+
+      <input
+        type="text"
+        name="subreddit"
+        class="ml-2 lg:mr-0 focus:outline-0 border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-500 rounded-lg bg-gray-200 py-2 px-4 block w-full lg:w-2/4 appearance-none leading-normal"
+        placeholder="Search Subreddit"
+      >
+
+      <button class="btn btn-primary ml-2">
+        Search
+      </button>
+    </header>
+
+    <div class="p-4 lg:p-6 w-full">
+      <h1 class="text-2xl">Post Title</h1>
+      <p class="text-xs text-gray-500 pb-1">
+        Posted by <span>15 hours ago</span>
+      </p>
+
+      <p class="pb-4">
+        Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to
+      </p>
+
+      <img class="m-auto" src="http://via.placeholder.com/300" alt="attachment">
     </div>
   </div>
 </template>
