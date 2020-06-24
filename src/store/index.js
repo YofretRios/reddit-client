b15187a7f97ebdc5efcaa6a147ead1095ba26e0f
Yofret Rios <yofret.rios@jobsity.com>
2020-06-24 11:42:50 -0500

    remove comments

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,6 @@
 import Vuex from 'vuex';
 import post from './modules/post';
 
-// const debug = process.env.NODE_ENV !== 'production';
-
-// const store = new Vuex.Store({
-//   modules: {
-//     post
-//   },
-//   strict: debug,
-// });
-
 export default () => {
   const debug = process.env.NODE_ENV !== 'production';
 
