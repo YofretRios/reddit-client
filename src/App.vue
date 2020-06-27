dec25694ba6ec9f79c3f4f76768875720141fe57
Yofret Rios <yofret.rios@jobsity.com>
2020-06-27 01:30:19 -0500

    fix: firefox and IE bugfix

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="h-screen">
-    <header class="flex fixed z-30 w-full p-4 lg:px-6 lg:py-4 flex border border-gray-200 bg-white">
+    <header class="flex justify-between fixed z-30 w-full p-4 lg:px-6 lg:py-4 flex border border-gray-200 bg-white">
       <button type="button" class="lg:hidden lg:px-3" @click="toggleNavBar">
-        <img src="./assets/hamburger.svg" class="max-w-none w-auto">
+        <img src="./assets/hamburger.svg" class="icon-height max-w-none w-auto">
       </button>
 
       <router-link class="ml-auto" to="/gallery">
-        <img src="./assets/gallery.svg" class="max-w-none">
+        <img src="./assets/gallery.svg" class="icon-height max-w-none">
       </router-link>
     </header>
     <div id="app" class="h-screen lg:flex">
--- src/Gallery.vue
+++ src/Gallery.vue
@@ -2,11 +2,11 @@
   <div class="h-screen">
     <header class="flex fixed z-30 w-full p-4 lg:px-6 lg:py-4 flex border border-gray-200 bg-white">
       <router-link to="/">
-        <img src="./assets/back.svg" class="max-w-none w-auto">
+        <img src="./assets/back.svg" class="icon-height max-w-none w-auto">
       </router-link>
 
       <router-link class="ml-auto" to="/gallery">
-        <img src="./assets/gallery.svg" class="max-w-none">
+        <img src="./assets/gallery.svg" class="icon-height max-w-none">
       </router-link>
     </header>
 
